da78a5bee3121754fba1e6f1c99b3887de27847d
SiddheshDaphane <siddhesh.daphane@gmail.com>
2026-03-27 19:47:17 -0500

    Used 2 hooks. UseState and UseEffect.

--- frontend/react/react-basics/src/App.jsx
+++ frontend/react/react-basics/src/App.jsx
@@ -1,39 +1,35 @@
+import { useEffect } from "react";
 import { useState } from "react"
 
 
 function App() {
-  return (
-  <div>
-    Hi there!
-    <Counter></Counter>
-  </div>
-  )
-}
 
-function Counter() {
-
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function increasecount() {
-    setCount(count + 1)
+    setCount(c => c + 1);
   }
 
   function decreasecount() {
-    setCount( count - 1)
+    setCount(c => c - 1);
   }
-
-  function resetcount() {
-    setCount(0)
-  }
-
+  
   return <div>
-    <h2>{count}</h2>
-    <button onClick={increasecount}>Increase 
-    Count</button>
+    <Counter count={count} />
+    <button onClick={increasecount}>Increase Count</button>
     <button onClick={decreasecount}>Decrease Count</button>
-    <button onClick={resetcount}>Reset Count</button>
   </div>
 }
 
+function Counter(props) {
+
+  useEffect(() => {
+    console.log("Count is increased")
+  },[props.count])
+
+  return <div>
+    Count is {props.count}
+  </div>
+}
 
 export default App
