61a56dbc15b7a5ee2e00a67d822938a70805e922
SiddheshDaphane <siddhesh.daphane@gmail.com>
2026-03-30 17:36:52 -0500

    Layout concept in react.

--- frontend/react/react-inter/src/App.jsx
+++ frontend/react/react-inter/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,18 +12,31 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
+            <Route path="/" element={<Landing />}></Route>
+          </Route>
         </Routes>
-        <Link to="/">Allen</Link>
-        |
-        <Link to="/neet/online-coaching-class-11">Class 11th</Link>
-        |
-        <Link to="/neet/online-coaching-class-12">Class 12th</Link>
+        
       </BrowserRouter>
     </div>
   )
+}
+
+function Layout() {
+  return <div>
+    Header
+    <br />
+  <Link to="/">Allen</Link>
+  |
+  <Link to="/neet/online-coaching-class-11">Class 11th</Link>
+  |
+  <Link to="/neet/online-coaching-class-12">Class 12th</Link>
+  <Outlet />
+  <br />
+  Footer
+  </div>
 }
 
 function Landing() {
