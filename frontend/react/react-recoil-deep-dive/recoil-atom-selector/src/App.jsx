9a982f32ba7c6e3e12b3dff4856607c250474375
SiddheshDaphane <siddhesh.daphane@gmail.com>
2026-04-08 17:37:39 -0500

    started recoil deep dive

---
+++ frontend/react/react-recoil-deep-dive/recoil-atom-selector/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MainApp />
+    </>
+  )
+}
+
+function MainApp() {
+  return <div>
+
+    <button>Home</button>
+    <button>My network</button>
+    <button>Jobs</button>
+    <button>Messaging</button>
+    <button>Notifications</button>
+
+  </div>
+}
+
+export default App
